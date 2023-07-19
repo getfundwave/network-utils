75fa530fd369f8f286067c8121fd3f87f1b68834
sabreen-parveen <sabreenparween2001@gmail.com>
2023-07-19 14:49:29 +0530

    enter password without prompt

--- aws-credentials/store-credentials-ubuntu.sh
+++ aws-credentials/store-credentials-ubuntu.sh
@@ -4,4 +4,4 @@ secret_access_key_id=$2
 profile=${3:-'internal'}
 
 password=$(echo "{ "AccessKeyId": "$access_key_id", "SecretAccessKey": "$secret_access_key_id" }" | base64)
-echo $password | secret-tool store --label="AWS Account Access Key-Pair" provider aws profile $profile -
+printf $password | secret-tool store --label="AWS Account Access Key-Pair" provider aws profile $profile
