796cda6b07c5cb33f36a035e3cd3fe6b912d7361
sabreen-parveen <sabreenparween2001@gmail.com>
2023-07-20 11:33:14 +0530

    rename files

--- aws-credentials/store-credentials.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-access_key_id=$1
-secret_access_key_id=$2
-profile=${3:-'internal'}
-
-password=$(echo "{ "AccessKeyId": "$access_key_id", "SecretAccessKey": "$secret_access_key_id" }" | base64)
-security add-generic-password -s "aws" -a $profile -w $password
