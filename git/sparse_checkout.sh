5b6b523082bce743f33852cdb51e5169b37f18c2
Srinivasan T M <=>
2024-01-22 11:46:46 +0530

    Added relevant comments, and modified help message

--- git/sparse_checkout.sh
+++ git/sparse_checkout.sh
@@ -4,16 +4,24 @@ URL=$1
 BRANCH=${2:-"main"}
 
 if [ -z "$1" ]; then
-  echo "Usage [REMOTE-URL]"
+  echo "Usage [REMOTE-URL] [BRANCH]"
+  echo "BRANCH argument is optional: defaults to main"
   exit 1
 fi
 
+# This specifies the remote URL from which to fetch from
 REMOTE="$(echo "$URL" | sed -n 's|\(.*github.com\)/\([^/]*\)/\([^/]*\)/\(.*\)|\1/\2/\3.git|p')"
+
+# This specifies the directory name that is to be created
 MAIN_DIR="$(echo "$URL" | sed -n 's|\(.*github.com\)/\([^/]*\)/\([^/]*\)/\(.*\)|\3|p')"
+
+# This specifies which subdirectory to selectively clone
 SUBDIR="$(echo "$URL" | sed -n 's|\(.*github.com\)/\([^/]*\)/\([^/]*\)/\(.*\)|\4|p')"
 
 test -d "$MAIN_DIR" || mkdir "$MAIN_DIR"
 cd "$MAIN_DIR" || exit
+
+# reference: https://stackoverflow.com/questions/600079/how-do-i-clone-a-subdirectory-only-of-a-git-repository
 test -d .git || git init --initial-branch="$BRANCH"
 if [ "$(git remote show | wc -l)" = "0" ]; then
   git remote add origin "$REMOTE"
@@ -25,4 +33,5 @@ else
   git sparse-checkout set "$SUBDIR"
 fi
 git pull origin "$BRANCH" --depth=1 --ff-only
+
 cd ..
