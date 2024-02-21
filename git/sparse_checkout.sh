af0343ec5565d17fb9109cb427f4a843731a20ad
Srinivasan T M <=>
2024-02-21 18:43:12 +0530

    Approaching sparse checkout in a different method

--- git/sparse_checkout.sh
+++ git/sparse_checkout.sh
@@ -1,37 +1,30 @@
 #!/bin/bash
 
-URL=$1
-BRANCH=${2:-"main"}
+REMOTE_URL=$1
+SUBDIR=$2
+BRANCH=${3:-"main"}
 
-if [ -z "$1" ]; then
-  echo "Usage [REMOTE-URL] [BRANCH]"
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Usage [REMOTE-URL] [SUBDIRECTORY] [BRANCH]"
   echo "BRANCH argument is optional: defaults to main"
   exit 1
 fi
 
-# This specifies the remote URL from which to fetch from
-REMOTE="$(echo "$URL" | sed -n 's|\(.*github.com\)/\([^/]*\)/\([^/]*\)/\(.*\)|\1/\2/\3.git|p')"
-
 # This specifies the directory name that is to be created
-MAIN_DIR="$(echo "$URL" | sed -n 's|\(.*github.com\)/\([^/]*\)/\([^/]*\)/\(.*\)|\3|p')"
+MAIN_DIR="$(echo "$REMOTE_URL" | sed -n 's|\(.*com\)/\([^/]*\)/\([^/]*\)|\3|p')"
 
-# This specifies which subdirectory to selectively clone
-SUBDIR="$(echo "$URL" | sed -n 's|\(.*github.com\)/\([^/]*\)/\([^/]*\)/\(.*\)|\4|p')"
+# reference: https://stackoverflow.com/questions/4114887/is-it-possible-to-do-a-sparse-checkout-without-checking-out-the-whole-repository
 
-test -d "$MAIN_DIR" || mkdir "$MAIN_DIR"
+test -d "$MAIN_DIR" || git clone --filter=blob:none --no-checkout --depth 1 --sparse "$REMOTE_URL"
 cd "$MAIN_DIR"
-
-# reference: https://stackoverflow.com/questions/600079/how-do-i-clone-a-subdirectory-only-of-a-git-repository
-test -d .git || git init --initial-branch="$BRANCH"
-if [ "$(git remote show | wc -l)" = "0" ]; then
-  git remote add origin "$REMOTE"
-fi
 git config core.sparseCheckout true
-if test -f .git/info/sparse-checkout; then 
-  git sparse-checkout add "$SUBDIR"
+if test -f .git/info/sparse-checkout; then
+  [ "$(grep -c "$SUBDIR" .git/info/sparse-checkout)" -eq 0 ] && git sparse-checkout add "$SUBDIR"
 else
   git sparse-checkout set "$SUBDIR"
 fi
-git pull origin "$BRANCH" --depth=1 --ff-only
+
+git checkout --force
+git pull origin "$BRANCH" --depth=1 --rebase
 
 cd ..
