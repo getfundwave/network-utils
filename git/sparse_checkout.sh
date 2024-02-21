7aa5a2808865092c69644e06f7685346f3507ce2
Srinivasan T M <tmsrinivasantm@gmail.com>
2024-02-21 22:09:48 +0530

    Fixed issue where running this script on a sparse-checkout branch with missing files did not restore the missing files + fixed issue where running this script did not pull remote changes to local

--- git/sparse_checkout.sh
+++ git/sparse_checkout.sh
@@ -17,14 +17,14 @@ MAIN_DIR="$(echo "$REMOTE_URL" | sed -n 's|\(.*com\)/\([^/]*\)/\([^/]*\)|\3|p')"
 
 test -d "$MAIN_DIR" || git clone --filter=blob:none --no-checkout --depth 1 --sparse "$REMOTE_URL"
 cd "$MAIN_DIR"
+
 git config core.sparseCheckout true
 if test -f .git/info/sparse-checkout; then
   [ "$(grep -c "$SUBDIR" .git/info/sparse-checkout)" -eq 0 ] && git sparse-checkout add "$SUBDIR"
 else
   git sparse-checkout set "$SUBDIR"
 fi
-
 git checkout --force
-git pull origin "$BRANCH" --depth=1 --rebase
+git pull origin "$BRANCH" --depth=1 --rebase --force
 
 cd ..
