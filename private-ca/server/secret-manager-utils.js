638733832783330b61d6f5361cd2d6c354004afd
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2025-06-24 13:59:29 +0530

    refactor: remove commented-out updateSecret function from secret-manager-utils

--- private-ca/server/secret-manager-utils.js
+++ private-ca/server/secret-manager-utils.js
@@ -7,18 +7,3 @@ export const getSecret = async (secretRegion, secretId) => {
   const secret = JSON.parse(response.SecretString);
   return secret;
 }
-
-
-/**
-export const updateSecret = async (secretRegion, secretId, key, value) => {
-  var secretsmanager = new aws.SecretsManager({ region: secretRegion });
-  let secret = await getSecret(secretId);
-  secret[key] = value;
-  var params = {
-    SecretId: secretId, 
-    SecretString: JSON.stringify(secret)
-  };
-  const updateRes = await secretsmanager.updateSecret(params).promise();
-  return updateRes;
-}
-**/
