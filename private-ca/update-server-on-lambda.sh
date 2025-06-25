b8054a69ce8ecd35f3b81ad53b66232aa7106354
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2025-06-25 12:14:08 +0530

    fix: remove output suppression from Lambda update command to show errors

--- private-ca/update-server-on-lambda.sh
+++ private-ca/update-server-on-lambda.sh
@@ -10,7 +10,9 @@ mv lambda.zip ../
 cd ..
 
 aws lambda update-function-code \
-    --function-name $FUNCTION_NAME \
-    --zip-file fileb://lambda.zip --region $REGION --profile $PROFILE >/dev/null 2>&1
+    --function-name "$FUNCTION_NAME" \
+    --zip-file fileb://lambda.zip \
+    --region "$REGION" \
+    --profile "$PROFILE"
 
 rm -r lambda.zip
